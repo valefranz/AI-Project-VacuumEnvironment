407d1ad2ae219623f4cb3a87bc96af6a9bdc2847
Mirco <mirco.theone@gmail.com>
2014-05-17 20:35:22 +0200

    Update agents_list.py

--- agents_list.py
+++ agents_list.py
@@ -1,6 +1,4 @@
 # -*- coding:utf-8 -*-
-
-import sys
 import inspect
 import importlib
 from os import path, walk
@@ -17,9 +15,10 @@ for root, dirs, files in walk("agents_dir"):
     for file_ in files:
         name, ext = path.splitext(file_)
         if ext == ".py" and name != "agents":
-            MODULES[name] =  importlib.import_module("agents_dir.{0}".format(name))
+            MODULES[name] = importlib.import_module(
+                "agents_dir.{0}".format(name))
             clsmembers = inspect.getmembers(MODULES[name], inspect.isclass)
             for class_name, class_ in clsmembers:
                 pkg, mod = path.splitext(class_.__module__)
                 if mod == ".{0}".format(name):
-                    ALL_AGENTS[name]= class_
+                    ALL_AGENTS[name] = class_
